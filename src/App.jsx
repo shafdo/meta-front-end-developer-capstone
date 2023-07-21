04cd794b4349cb0e1cb56d14faf3b82ff1f6c234
Shalinda Fernando <yugantha1468@gmail.com>
2023-07-22 00:47:44 +0530

    Routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
-import Header from './components/Header/'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <>
-      <Header />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
